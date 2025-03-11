80dba86b6c1216b678cf4dd7e2b7a8c521b43f6b
Chandra Prakash <spamsofcp@gmail.com>
2025-03-11 16:59:44 +0530

    modifications

--- dv/mpd/readme.md
+++
@@ -1,1 +0,0 @@
-[text](../prt_v2/readme.md)
--- dv/prt_v2/prt_sequencer.sv
+++
@@ -1,7 +0,0 @@
-class prt_sequencer extends uvm_sequencer #(prt_transaction);
-  `uvm_component_utils(prt_sequencer)
-  
-  function new(string name, uvm_component parent);
-    super.new(name, parent);
-  endfunction
-endclass
