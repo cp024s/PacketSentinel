55587d52155b7f40920d0c1760ebcac184fa5e08
Chandra Prakash <spamsofcp@gmail.com>
2025-03-20 16:57:25 +0530

    Changes

--- scripts/implement.tcl
+++
@@ -1,8 +0,0 @@
-open_project PRT_Project
-opt_design
-place_design
-route_design
-write_checkpoint -force reports/implementation.dcp
-report_timing_summary -file reports/timing_impl.rpt
-report_utilization -file reports/utilization_impl.rpt
-exit
--- scripts/synthesize.tcl
+++
@@ -1,8 +0,0 @@
-open_project PRT_Project
-read_verilog PRT.sv
-read_verilog dual_port_bram.sv
-synth_design -top PRT -part xc7a200tfbg676-2
-write_checkpoint -force reports/synthesis.dcp
-report_timing_summary -file reports/timing_synth.rpt
-report_utilization -file reports/utilization_synth.rpt
-exit
